0f410b0f666bcdd353560a94a8f3131eefab1a6b
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-03-18 05:49:38 -0300

    [ADD] Diseño final terminado, falta responsive

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+//ImgFlayer
+
 function myFunction() {
-    var element = document.getElementById("imgLive");
-    element.classList.add("pulse");
-  }
+  var element = document.getElementById("imgLive");
+  element.classList.add("animate__pulse");
+  element.addEventListener("animationend", function() {
+    element.classList.remove("animate__pulse");
+  });
+}
+var element = document.getElementById("imgLive");
+element.addEventListener("mouseover", myFunction);
+
+
+// Cards
+
+var cards = document.querySelectorAll('.card');
+cards.forEach(function(card) {
+  card.addEventListener('mouseover', function() {
+    card.classList.add('animate__animated', 'animate__bounceIn');
+  });
+  card.addEventListener('animationend', function() {
+    card.classList.remove('animate__animated', 'animate__bounceIn');
+  });
+});
